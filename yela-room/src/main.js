9e653811b16fad377119c6795968e5ee02d2a51a
Yelaman Moldagali <115030149+yelagman@users.noreply.github.com>
2025-09-28 15:53:19 +0400

    Added Buttons
    In this commit, I added working buttons for My Work, About, and Contact Sections. I sitll have to work on the SCSS, but the basic functionality of these buttons is accomplished.

--- yela-room/src/main.js
+++ yela-room/src/main.js
@@ -3,12 +3,66 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import gsap from "gsap";
 
 const canvas = document.querySelector("#experience-canvas");
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
 }
+
+const modals = {
+  work: document.querySelector(".modal.work"),
+  about: document.querySelector(".modal.about"),
+  contact: document.querySelector(".modal.contact"),
+}
+
+document.querySelectorAll(".modal-exit-button").forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const modal = e.target.closest('.modal');
+    hideModal(modal);
+  });
+});
+
+const showModal = (modal) => {
+  modal.style.display = "block";
+
+  gsap.set(modal, {opacity: 0});
+
+  gsap.to(modal, {
+    opacity: 1,
+    duration: 0.5,
+  
+  });
+};
+
+const hideModal = (modal) => {
+
+  gsap.to(modal, {
+    opacity: 0,
+    duration: 0.5,
+    onComplete: () => {
+      modal.style.display = "none";
+    },
+
+  });
+};
+
+
+
+const yAxisFans = [];
+const raycasterObjects = [];
+let currentIntersects = [];
+
+const socialLinks = {
+  "Github": 'https://github.com/yelagman',
+  "LinkedIn": 'https://www.linkedin.com/in/yelaman-moldagali/',
+  "Instagram": 'https://www.instagram.com/jibaliba/',
+  'Scholar': 'https://dl.acm.org/profile/99660897391',
+}
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
 // loaders
 const textureLoader = new THREE.TextureLoader();
@@ -68,16 +122,60 @@ const videoTexture = new THREE.VideoTexture(videoElement);
 videoTexture.colorSpace = THREE.SRGBColorSpace;
 videoTexture.flipY = false;
 
+
+window.addEventListener('mousemove', (e) => {
+  pointer.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+})
+
+window.addEventListener('click', (e) => {
+
+  if (currentIntersects.length > 0) {
+    const object = currentIntersects[0].object;
+
+     Object.entries(socialLinks).forEach(([key, url]) => {
+      if (object.name.includes(key)) {
+        const newWindow = window.open();
+        newWindow.opener = null;
+        newWindow.location = url;
+        newWindow.target = "_blank";
+        newWindow.rel = "noopener noreferrer";
+      }
+     });
+
+
+    if (object.name.includes('My_Work')){
+      showModal(modals.work);
+    } else if (object.name.includes('About')) {
+      showModal(modals.about);
+    } else if (object.name.includes('Contact')) {
+      showModal(modals.contact);
+    }
+
+
+  }
+});
+
 // replace your loader.load with this
 loader.load('/models/Room_Portfolio_V1.glb', (glb) => {
 
   glb.scene.traverse((child) => {
     if (child.isMesh) {
 
+      if (child.name.includes('Raycaster')){
+        raycasterObjects.push(child);
+      }
+
       if (child.name.includes('Computer_Screen')){
         child.material = new THREE.MeshBasicMaterial({
           map: videoTexture,
         })
+      }
+
+      if (child.name.includes("Fan")){
+        if (child.name.includes("Fan_1") || child.name.includes("Fan_2")){
+          yAxisFans.push(child);
+        }
       }
 
       // Your existing textureMap logic
@@ -164,6 +262,35 @@ const render = () =>{
   // console.log(camera.position);
   // console.log("000000000");
   // console.log(controls.target);
+
+  // animating fans
+  yAxisFans.forEach((fan) => {
+    fan.rotation.y += 0.01;
+  })
+
+  raycaster.setFromCamera( pointer, camera );
+
+	// calculate objects intersecting the picking ray
+	currentIntersects = raycaster.intersectObjects( raycasterObjects );
+
+	for ( let i = 0; i < currentIntersects.length; i ++ ) {
+		currentIntersects[ i ].object.material.color.set( 0xff0000 );
+  }
+
+  if (currentIntersects.length > 0) {
+    
+    const currentIntersectObject = currentIntersects[0].object;
+
+    if (currentIntersectObject.name.includes('Pointer')){
+      
+      document.body.style.cursor = "pointer";
+    } else {
+        document.body.style.cursor = "default";
+      }
+  } else {
+      document.body.style.cursor = "default";
+      }
+
 
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
